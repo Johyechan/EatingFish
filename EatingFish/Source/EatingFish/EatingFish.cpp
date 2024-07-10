afa9a391f0e74f8a18b9bd50262b35fb312814b0
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-07-10 15:49:03 +0900

    Make Project

---
+++ EatingFish/Source/EatingFish/EatingFish.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EatingFish.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EatingFish, "EatingFish" );
---
+++ EatingFish/Source/EatingFish/EatingFishGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EatingFishGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EATINGFISH_API AEatingFishGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
