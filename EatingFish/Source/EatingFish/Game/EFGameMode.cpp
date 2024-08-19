39bab7670eacb1816a147a1320827ae056243e67
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-08-19 16:38:31 +0900

    Make Character 1

--- EatingFish/Source/EatingFish/Game/EFGameMode.cpp
+++ EatingFish/Source/EatingFish/Game/EFGameMode.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Game/EFGameMode.h"
+#include "EFGameMode.h"
 
+AEFGameMode::AEFGameMode()
+{
+	// DefaultPawnClass // 여기 바꿔야 하나?
+	static ConstructorHelpers::FClassFinder<APawn>ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+
+	static ConstructorHelpers::FClassFinder<APawn>DefaultPawnClassRef(TEXT("/Script/EatingFish.EFCharacterPlayer"));
+	if (DefaultPawnClassRef.Class)
+	{
+		DefaultPawnClass = DefaultPawnClassRef.Class;
+	}
+
+	// PlayerControllerClass
+	static ConstructorHelpers::FClassFinder<APlayerController>PlayerControllerClassRef(TEXT("/Script/EatingFish.EFPlayerController"));
+	if (PlayerControllerClassRef.Class)
+	{
+		PlayerControllerClass = PlayerControllerClassRef.Class;
+	}
+}
---
+++ EatingFish/Source/EatingFish/Player/EFPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EFPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EATINGFISH_API AEFPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
